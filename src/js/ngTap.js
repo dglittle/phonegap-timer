7840ce121c64ff9879e38515edfeb85d3601d75c
Sergei Z. <zholudev.s@gmail.com>
2013-08-11 22:55:07 +0200

    bugfixing

---
+++ src/js/ngTap.js
@@ -0,0 +1,20 @@
+angular.module('timer').directive('ngTap', function() {
+  return function(scope, element, attrs) {
+      var tapping;
+      tapping = false;
+      element.bind('touchstart', function(e) {
+          element.addClass('active');
+          tapping = true;
+        });
+      element.bind('touchmove', function(e) {
+        element.removeClass('active');
+        tapping = false;
+      });
+      element.bind('touchend', function(e) {
+      element.removeClass('active');
+      if (tapping) {
+        scope.$apply(attrs['ngTap'], element);
+      }
+    });
+  };
+});
